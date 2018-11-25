13064fcd95f3c2db0779db392076658f6ebca174
yinseng2001 <seng655615>
2018-11-25 20:52:55 +0700

    plot data by location

--- core/trainer.py
+++ core/trainer.py
@@ -1,84 +1,45 @@
-import keras
-from keras.utils import multi_gpu_model
-from keras.preprocessing.image import ImageDataGenerator
-import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
-import tensorflow as tf
-from keras import backend as K
-from model.VGG16 import VGG16
-import pickle
-num_cores = 4
-num_GPU = 0
-num_CPU = 4
-# config = tf.ConfigProto(intra_op_parallelism_threads=num_cores,\
-#         inter_op_parallelism_threads=num_cores, allow_soft_placement=True,\
-#         device_count = {'CPU' : num_CPU, 'GPU' : num_GPU})
-# config.gpu_options.allow_growth=True
-# sess = tf.Session(config=config)
-# K.set_session(sess)
-
-BS = 120
-
-
+import matplotlib.pyplot as plt
 # Model support
 def train_model():
-  train_datagen = ImageDataGenerator(
-    rescale = 1./255,
-    shear_range = 0.1,
-    zoom_range = 0.1,
-    horizontal_flip = True,
-    fill_mode="nearest")
+      
+  df = pd.read_csv("dataset/flood_data.csv", sep = ",",usecols=['created_at','sensor_location','streamHeight'])
+  # df['updated_at'] = pd.to_datetime(df['updated_at'],infer_datetime_format=True)
 
-  test_datagen = ImageDataGenerator(rescale = 1./255)
-  # load train data 
-  training_set = train_datagen.flow_from_directory(
-    'dataset/train',
-    target_size = (64, 64),
-    batch_size = BS,
-    shuffle=True,
-    seed = 7,
-    class_mode = 'categorical')
-  # load validation data
-  validation_set = test_datagen.flow_from_directory(
-    'dataset/validation',
-    target_size = (64, 64),
-    batch_size = BS,
-    shuffle=True,
-    seed = 7,
-    class_mode = 'categorical')
+  df['created_at'] = pd.to_datetime(df['created_at'],utc=False)
+  df.sort_values(by='created_at',ascending=True,inplace=True,na_position='first') # This
+  # df = df.cumsum()
+  print(df.head(20))
 
-  print(validation_set.class_indices)
-  print(validation_set.classes)
-  print(validation_set.num_classes)
+  df.set_index('created_at')
 
-  imgs,labels = next(training_set)
-  print(labels)
-
-  file_Name = 'label_match.pickle'
-  fileObject = open(file_Name,'wb') 
-  # this writes the object a to the file named 'testfile'
-  pickle.dump(validation_set.class_indices,fileObject)   
-  # here we close the fileObject
-  fileObject.close()
+  df_sr = df.query('sensor_location == "Siemreap"')
+  df_bbt = df.query('sensor_location == "Battambang"')
+  df_kc = df.query('sensor_location == "Kampong Chhnang"')
+  df_ps = df.query('sensor_location == "Pursat"')
 
 
-  nClasses = validation_set.num_classes
+  print(df_sr.head(20))
 
-  print('nClasses',nClasses)
-  model = VGG16().create_model((64, 64,3),nClasses)
-  model.compile(optimizer = 'adam', loss = 'categorical_crossentropy', metrics = ['accuracy'])
-  model.fit_generator(training_set,
-    steps_per_epoch = (1200/BS),
-    nb_epoch = 20,
-    validation_data = validation_set,
-    verbose=1)
-
-
-  # save the model to disk
-  print("[INFO] serializing network...")
-  model.save('lenet.model')
-  model.save_weights('weights.h5')
+  df_sr.plot(x='created_at',y='streamHeight',title='Siemreap').figure.savefig('output/sr.png')
+  df_bbt.plot(x='created_at',y='streamHeight',title='Battambang').figure.savefig('output/btb.png')
+  df_kc.plot(x='created_at',y='streamHeight',title='Kampong Chhnang').figure.savefig('output/kc.png')
+  df_ps.plot(x='created_at',y='streamHeight',title='Pursat').figure.savefig('output/ps.png')
+  plt.show()
     
 if __name__ == "__main__":
+      
+    # df = pd.DataFrame( 
+    #   {
+    #     'Symbol':['A','A','A'] ,
+    #     'Date':['02/20/2015','01/15/2016','08/21/2015']
+    #   })
+    # print(df)
+
+    # df['Date'] =pd.to_datetime(df.Date)
+    # df.sort_values(by='Date',inplace=True, ascending=True)
+    # print(df)
+
     # construct the argument parse and parse the arguments
     train_model()
