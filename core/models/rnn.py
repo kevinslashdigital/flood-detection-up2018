cbc5500dde53a609f89f2d32574721021a0a118c
kevin_slash <seng655615>
2018-11-30 17:47:11 +0700

    LSTM

---
+++ core/models/rnn.py
@@ -0,0 +1,16 @@
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+
+class RNN_LSTM():
+  def __init__(self):
+    pass
+
+  def create_model(self,batch_size,input_shape,nClasses):
+    
+    model = Sequential()
+    model.add(LSTM(32,input_shape=input_shape))
+    # model.add(LSTM(32,batch_input_shape=(batch_size, input_shape[1], input_shape[2]), return_sequences=False,stateful=True))
+    model.add(Dense(nClasses))
+    
+    return model
